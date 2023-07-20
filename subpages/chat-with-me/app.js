00dc945c13f4ce5d3b59804b1cc519cb96596d9e
jialefu <213200900@seu.edu.cn>
2023-07-20 20:34:18 +0800

    给ChatGLM增加了看上下文的功能

--- subpages/chat-with-me/app.js
+++ subpages/chat-with-me/app.js
@@ -3,6 +3,8 @@ const inputEl = document.getElementById('inputbox');
 const submitEl = document.getElementById('submit');
 const loadingEl = document.getElementById('loading');
 
+prompt = []
+
 submitEl.addEventListener('click', async () => {
   const input = inputEl.innerText;
   addMessage(input, 'user');
@@ -12,7 +14,10 @@ submitEl.addEventListener('click', async () => {
   loadingEl.style.display = 'block';
 
   // 使用 OpenAI API 获取 ChatGPT 的回答
-  const response = await getResponseFromAPI(input);
+  
+  prompt.push({"role": "user", "content": input});
+  const response = await getResponseFromAPI(prompt);
+  prompt.push({"role": "assistant", "content": response});
 
   // 隐藏加载动画
   loadingEl.style.display = 'none';
@@ -34,40 +39,7 @@ function addMessage(text, sender) {
   chatboxEl.scrollTop = chatboxEl.scrollHeight;
 }
 
-// async function getResponseFromAPI(input) {
-//     const prompt = [{"role": "user", "content": input}];
-//     const endpoint = 'https://open.bigmodel.cn/api/paas/v3/model-api/chatglm_std/sse-invoke';
-
-//     const response = await fetch(endpoint, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': 'eyJhbGciOiJIUzI1NiIsInNpZ25fdHlwZSI6IlNJR04iLCJ0eXAiOiJKV1QifQ.eyJhcGlfa2V5IjoiNWExM2I4ZGYyMDNlYjY1NDY0MjU4MWJhZTYwN2QwMTgiLCJleHAiOjE2ODQxOTM2NzIxMzUwMjYsInRpbWVzdGFtcCI6MTY4OTg0MzAwNTAyNn0.NMd5RCQ1e3cpqp2pRIBlG7_zdmhuFZuOcVnY0SFQn54'
-//         },
-
-//         body: JSON.stringify({
-//             model: "chatglm_std",
-//             prompt,
-//         }),
-//     });
-
-//     const result = await response.json();
-
-//     let finalMessage = '';
-  
-//     result.forEach(event => {
-//         if (event.event === 'add') {
-//           finalMessage += event.data;
-//         }
-//     });
-  
-//     console.log(finalMessage); 
-    
-//     return finalMessage;
-// }
-
-async function getResponseFromAPI(input) {
-  const prompt = [{"role": "user", "content": input}];
+async function getResponseFromAPI(prompt) {
   const endpoint = 'https://open.bigmodel.cn/api/paas/v3/model-api/chatglm_std/sse-invoke';
 
   const rawResponse = await fetch(endpoint, {
