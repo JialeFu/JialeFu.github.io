78c1fe5d2cf08d72fe7c7949ebc2c72abdcba508
jialefu <213200900@seu.edu.cn>
2023-07-20 20:13:06 +0800

    实现了ChatGLM api的调用

--- subpages/chat-with-me/app.js
+++ subpages/chat-with-me/app.js
@@ -20,15 +20,6 @@ submitEl.addEventListener('click', async () => {
   addMessage(response, 'chatgpt');
 });
 
-// function addMessage(text, sender) {
-//   const messageEl = document.createElement('div');
-//   messageEl.classList.add('message');
-//   messageEl.classList.add(`${sender}-message`);
-//   messageEl.innerHTML = text;
-//   chatboxEl.appendChild(messageEl);
-//   chatboxEl.scrollTop = chatboxEl.scrollHeight;
-// }
-
 function addMessage(text, sender) {
   const messageEl = document.createElement('div');
   messageEl.classList.add('message');
@@ -43,27 +34,100 @@ function addMessage(text, sender) {
   chatboxEl.scrollTop = chatboxEl.scrollHeight;
 }
 
+// async function getResponseFromAPI(input) {
+//     const prompt = [{"role": "user", "content": input}];
+//     const endpoint = 'https://open.bigmodel.cn/api/paas/v3/model-api/chatglm_std/sse-invoke';
+
+//     const response = await fetch(endpoint, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization': 'eyJhbGciOiJIUzI1NiIsInNpZ25fdHlwZSI6IlNJR04iLCJ0eXAiOiJKV1QifQ.eyJhcGlfa2V5IjoiNWExM2I4ZGYyMDNlYjY1NDY0MjU4MWJhZTYwN2QwMTgiLCJleHAiOjE2ODQxOTM2NzIxMzUwMjYsInRpbWVzdGFtcCI6MTY4OTg0MzAwNTAyNn0.NMd5RCQ1e3cpqp2pRIBlG7_zdmhuFZuOcVnY0SFQn54'
+//         },
+
+//         body: JSON.stringify({
+//             model: "chatglm_std",
+//             prompt,
+//         }),
+//     });
+
+//     const result = await response.json();
+
+//     let finalMessage = '';
+  
+//     result.forEach(event => {
+//         if (event.event === 'add') {
+//           finalMessage += event.data;
+//         }
+//     });
+  
+//     console.log(finalMessage); 
+    
+//     return finalMessage;
+// }
+
 async function getResponseFromAPI(input) {
-    const endpoint = 'https://api.openai.com/v1/completions';
-    const apiKey = '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'; //换成自己的API Key
-    const prompt = input;
+  const prompt = [{"role": "user", "content": input}];
+  const endpoint = 'https://open.bigmodel.cn/api/paas/v3/model-api/chatglm_std/sse-invoke';
 
-    const response = await fetch(endpoint, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
-        },
+  const rawResponse = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'eyJhbGciOiJIUzI1NiIsInNpZ25fdHlwZSI6IlNJR04iLCJ0eXAiOiJKV1QifQ.eyJhcGlfa2V5IjoiNWExM2I4ZGYyMDNlYjY1NDY0MjU4MWJhZTYwN2QwMTgiLCJleHAiOjE2ODQxOTM2NzIxMzUwMjYsInRpbWVzdGFtcCI6MTY4OTg0MzAwNTAyNn0.NMd5RCQ1e3cpqp2pRIBlG7_zdmhuFZuOcVnY0SFQn54'
+      },
 
-        body: JSON.stringify({
-            model: "text-davinci-003",
-            prompt,
-            max_tokens: 100,
-            n: 1,
-            stop: null,
-            temperature: 0.5,
-        }),
-    });
-    const result = await response.json();
-    return result.choices[0].text;
+      body: JSON.stringify({
+          model: "chatglm_std",
+          prompt,
+      }),
+  });
+
+  const responseText = await rawResponse.text();
+  const result = convertToJSON(responseText);
+
+  let finalMessage = '';
+
+  result.forEach(event => {
+      if (event.event === 'add') {
+        finalMessage += event.data;
+      }
+  });
+
+  console.log(finalMessage); 
+  
+  return finalMessage;
+}
+
+function convertToJSON(data) {
+  const lines = data.split('\n');
+
+  let jsonArray = [];
+  let jsonObject = {};
+
+  for (let i = 0; i < lines.length; i++) {
+      const line = lines[i].trim();
+      if (!line) continue;
+
+      const [key, value] = line.split(':');
+
+      if (key === 'event' && Object.keys(jsonObject).length !== 0) {
+          jsonArray.push(jsonObject);
+          jsonObject = {};  
+      }
+
+      try {
+          // Try to parse the value as JSON
+          jsonObject[key] = JSON.parse(value);
+      } catch (e) {
+          // If it fails, just use the raw value
+          jsonObject[key] = value;
+      }
+  }
+
+  if (Object.keys(jsonObject).length !== 0) {
+      jsonArray.push(jsonObject);
+  }
+
+  return jsonArray;
 }
